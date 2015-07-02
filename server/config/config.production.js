bd9e5d0549e1e27aa06d6420fe1a6eb951cf1e30
paul van bladel <paul.vanbladel@gmail.com>
2015-07-02 13:25:41 +0200

    config file security

--- server/config/config.production.js
+++ server/config/config.production.js
@@ -3,17 +3,17 @@ var config = require('./config.global');
 config.env = "production";
 config.port = 10000;
 config.mongo = {};
-config.mongo.connectionstring = 'mongodb://localhost/' + 'aureliauth-production' ;
-TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
-MONGO_URI: process.env.MONGO_URI || 'localhost',
-FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook Client Secret',
-FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
-GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
-GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
-LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
-WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
-TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
-TWITTER_SECRET: process.env.TWITTER_SECRET || 'Twitter Consumer Secret',
-TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'Twitter Callback URL',
-YAHOO_SECRET: process.env.YAHOO_SECRET || 'Yahoo Client Secret'
+config.mongo.connectionstring = 'mongodb://localhost/' + 'aureliauth-production';
+config.TOKEN_SECRET = process.env.TOKEN_SECRET || 'A hard to guess string';
+config.MONGO_URI = process.env.MONGO_URI || 'localhost';
+config.FACEBOOK_SECRET = process.env.FACEBOOK_SECRET || 'Facebook Client Secret';
+config.FOURSQUARE_SECRET = process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret';
+config.GOOGLE_SECRET = process.env.GOOGLE_SECRET || 'Google Client Secret';
+config.GITHUB_SECRET = process.env.GITHUB_SECRET || 'GitHub Client Secret';
+config.LINKEDIN_SECRET = process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret';
+config.WINDOWS_LIVE_SECRET = process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret';
+config.TWITTER_KEY = process.env.TWITTER_KEY || 'Twitter Consumer Key';
+config.TWITTER_SECRET = process.env.TWITTER_SECRET || 'Twitter Consumer Secret';
+config.TWITTER_CALLBACK = process.env.TWITTER_CALLBACK || 'Twitter Callback URL';
+config.YAHOO_SECRET = process.env.YAHOO_SECRET || 'Yahoo Client Secret';
 module.exports = config;
