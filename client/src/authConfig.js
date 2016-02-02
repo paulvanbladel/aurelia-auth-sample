1b3d073705e5c836adeadbd26403b1813383430d
paul van bladel <paul.vanbladel@gmail.com>
2016-02-02 16:04:24 +0100

    line up with beta 1.1 framework

--- client/src/authConfig.js
+++ client/src/authConfig.js
@@ -1,7 +1,11 @@
 var configForDevelopment = {
 	providers: {
 		google: {
-			clientId: '239531826023-ibk10mb9p7ull54j55a61og5lvnjrff6.apps.googleusercontent.com'
+			clientId: '239531826023-ibk10mb9p7ull54j55a61og5lvnjrff6.apps.googleusercontent.com',
+			state: function(){
+                var val = ((Date.now() + Math.random()) * Math.random()).toString().replace(".", "");
+                return encodeURIComponent(val);
+            }
 		} 
 		,
 		linkedin:{
