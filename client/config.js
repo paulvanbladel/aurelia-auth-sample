db939cbb248ac37852a035f604be8d89db2db738
paul van bladel <paul.vanbladel@gmail.com>
2016-05-31 08:46:47 +0200

    chore(package): using aurelia-auth v3.0.1

--- client/config.js
+++ client/config.js
@@ -15,7 +15,7 @@ System.config({
 
   map: {
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.0-beta.1.2.1",
-    "aurelia-auth": "npm:aurelia-auth@2.2.1",
+    "aurelia-auth": "npm:aurelia-auth@3.0.1",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1.2.1",
     "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.2.5",
     "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.2.5",
@@ -92,12 +92,11 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.2.2",
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.2.7"
     },
-    "npm:aurelia-auth@2.2.1": {
+    "npm:aurelia-auth@3.0.1": {
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.2.3",
       "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0-beta.1.2.1",
       "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.2.5",
-      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.2.3",
-      "isomorphic-fetch": "npm:isomorphic-fetch@2.2.1"
+      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.2.3"
     },
     "npm:aurelia-binding@1.0.0-beta.1.3.6": {
       "aurelia-logging": "npm:aurelia-logging@1.0.0-beta.1.2.1",
